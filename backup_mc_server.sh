6b698efd73c001d869190cea7c7e54f18d16a067
yindaheng98 <l>
2020-01-25 15:30:34 +0800

    更新

--- backup_mc_server.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-backupfile_name="mcserver_backup/mcsbkp""`date +%Y%m%d`""-""`date +%H%m%S`"".tar.gz"
-tar -cvf $backupfile_name mcserver/
--- start_mc_server.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /home/yin/mcserver
-java -Xms2G -Xmx2G -jar spigot-1.13.2.jar nogui
